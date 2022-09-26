af9f8d0a3be698d592d8bdf1f8f5d41110afc48a
BenchZJ <252487266@qq.com>
2022-09-26 20:50:35 +0800

    修改接线位置

--- 08_中断系统点亮LED/main.c
+++ 08_中断系统点亮LED/main.c
@@ -5,8 +5,9 @@
 typedef unsigned char u8;
 typedef unsigned int u16;
 
-#define S1 0x09
+sbit S1 = P1^0;
 sbit LED = P0^0;
+
  
 void delay_10us(unsigned int ten_us) 
 {
@@ -19,23 +20,18 @@ void init_exit0() {
     IT0=1;
 }
 
-
-void check_s1() {
-   if(P1 == S1) {
-      INT0 = 1;
-   }
-}
-
+ 
  void main() 
 {
      init_exit0();
+     S1 = 1;
      while(1) {
-        check_s1();
+      
      }
     
 }
 
 void exit0() interrupt 0 {
-       delay_10us(1000);
+       delay_10us(100);
        LED = !LED;
 }
